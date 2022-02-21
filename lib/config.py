446502279545604976ff36297032386c5b7948b8
CKA <18747836+cccorn@users.noreply.github.com>
2022-02-22 03:55:38 +0800

    Create config.py

---
+++ lib/config.py
@@ -0,0 +1,8 @@
+refresh=0
+threshold_epsilon=1e-3
+# Basis Clifford Gates
+clifford_gates = ['i','id','iden','x','y','z','h','s','sdg','sinv','v','w','cx','cz','swap']
+non_clifford_gates = ['t','tdg','rz','ry']
+perturbation=1
+perturbation_epsilon=1e-4
+cache=dict()
